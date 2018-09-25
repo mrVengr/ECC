dd8f5fa80285c962548e18fd53918e5831fb9408
mrVengr <jocoala@mail.ru>
2018-09-26 00:51:25 +0300

    +2D EC, changed algorythm

--- EC_draw.py
+++ EC_draw.py
@@ -2,7 +2,7 @@ from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_ec(fn, bbox=(-3,3)):
+def plot_ec(fn, bbox=(-4,4)):
 
     xmin, xmax, ymin, ymax, zmin, zmax = bbox*3
     fig = plt.figure()
@@ -33,32 +33,52 @@ def plot_ec(fn, bbox=(-3,3)):
     plt.show()
 
 def calc_points(A,b,p):
-    Y = []
+    Ys = []
     X1r=[]
     X2r=[]
     Yr= []
     Points=(X1r,X2r,Yr)
+    count=0
     for i in range(p):
-        Y.append(i**2)
+        Ys.append((i**2)%p)
+    Y=np.array(Ys)
     for x1 in range(p):
         for x2 in range(p):
-            ys=(x1**3+x2**3+A[0]*x1+A[1]*x2+b)%p
-            try:
-                yr=Y.index(ys)
+            yc=(x1**3+x2**3+A[0]*x1+A[1]*x2+b)%p
+            yarr=np.where(Y==yc)[0]
+            for i in yarr:
+                count+=1
                 X1r.append(x1)
                 X2r.append(x2)
-                Yr.append(yr)
-                if yr!=0:
-                    X1r.append(x1)
-                    X2r.append(x2)
-                    Yr.append(p-yr)
-            except: 
-                pass
-    return Points
+                Yr.append(i)
+                print(x1,x2,i)
+    return (Points,count)
 
 def plot_ec_on_ff(A,b,p):
-    p=calc_points(A,b,p)
+    f=calc_points(A,b,p)
+    p=f[0]
+    count=f[1]
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    cset = ax.scatter(p[0],p[1],p[2],marker='.',depthshade=True)
+    cset = ax.scatter(p[0],p[1],p[2],marker='o',depthshade=True)
+    plt.title(count)
+    plt.show()
+
+def plot_2D_ec(a,b,p):
+    Ys =[]
+    Xr=[]
+    Yr=[]
+    count=0
+    for i in range(p):
+        Ys.append((i**2)%p)
+    Y=np.array(Ys)
+    for x in range(p):
+        yc=(x**3+a*x+b)%p
+        yarr=np.where(Y==yc)[0]
+        for i in yarr:
+            count+=1
+            Xr.append(x)
+            Yr.append(i)
+    plt.scatter(Xr,Yr,marker='.')
+    plt.title(count)
     plt.show()
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import EC_draw as ec
 
 def eliptic_curve(x,y,z):
-    return x**3+y**3-x-y-z**2
-# ec.plot_ec(eliptic_curve) # ec on field nєR 
+    return x**3+y**3-x-2*y+1-z**2
+ec.plot_ec(eliptic_curve) # ec on field nєR 
 
-ec.plot_ec_on_ff([-1,-1],0,7) # ec on finite field
+ec.plot_ec_on_ff([-1,-2],1,17) # ec on finite field
+#ec.plot_2D_ec(-1,1,7) # 2D ec
+
